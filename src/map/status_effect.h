891235db68b807a583b631265a743e8bf81aa6d0
Umeboshi <terrandragon@gmail.com>
2023-04-11 18:10:21 -0700

    Increase mob spell list limit and status effect limit. See: https://github.com/AirSkyBoat/AirSkyBoat/pull/46

--- src/map/status_effect.h
+++ src/map/status_effect.h
@@ -758,8 +758,7 @@ enum EFFECT
     // EFFECT_PLACEHOLDER           = 1023 // The client dat file seems to have only this many "slots", results of exceeding that are untested.
 };
 
-#define MAX_EFFECTID 807 // 768 real + 39 custom
-
+#define MAX_EFFECTID 1023 // 768 real + 232 custom
 /************************************************************************
  *                                                                       *
  *  Нерешенные задачи:                                                   *
