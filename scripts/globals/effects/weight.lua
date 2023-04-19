dd29c0e897a520070da0911bfc3d02971941beda
Umeboshi <terrandragon@gmail.com>
2023-04-18 17:11:20 -0700

    Gravity Evasion Debuff

--- scripts/globals/effects/weight.lua
+++ scripts/globals/effects/weight.lua
@@ -7,6 +7,7 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.MOVE, -effect:getPower())
+    target:addMod(xi.mod.EVA, -10)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -14,6 +15,7 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.MOVE, -effect:getPower())
+    target:delMod(xi.mod.EVA, -10)
 end
 
 return effectObject
