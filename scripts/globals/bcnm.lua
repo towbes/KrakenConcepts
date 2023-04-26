10086a83e401ee655dcc2225186b727282bf1343
Umeboshi <terrandragon@gmail.com>
2023-04-26 00:11:41 -0700

    KSNM30 Prehistoric Pigeons https://github.com/AirSkyBoat/AirSkyBoat/pull/215/

--- scripts/globals/bcnm.lua
+++ scripts/globals/bcnm.lua
@@ -284,7 +284,7 @@ local battlefields =
         { 15,   79, 1130 },   -- Up in Arms (BS60)
         { 16,   80, 1175 },   -- Copycat (KS30)
     --  { 17,   81, 1178 },   -- Operation Desert Swarm (KS30) -- TODO: Wild Rage gets stronger as they die. Build sleep resistance. Testing.
-    --  { 18,   82, 1180 },   -- Prehistoric Pigeons (KS30) -- TODO: Build resistance to sleep quickly. When one dies, remaining ones become more powerful.
+        { 18,   82, 1180 },   -- Prehistoric Pigeons (KS30)
     --  { 19,   83, 3351 },   -- The Palborough Project (KC30)
     --  { 20,   84, 3352 },   -- Shell Shocked (KC50)
         { 21,   85,    0 },   -- Beyond Infinity (Quest)
--- sql/mob_groups.sql
+++ sql/mob_groups.sql
@@ -1276,7 +1276,7 @@ INSERT INTO `mob_groups` VALUES (58,6197,33,'Ulaern_brd',960,0,2514,0,0,70,73,0)
 INSERT INTO `mob_groups` VALUES (59,6194,33,'Ulaern_pld',960,0,2514,0,0,70,73,0);
 INSERT INTO `mob_groups` VALUES (60,7033,33,'Omhpemde_nodive',960,0,1854,0,0,73,77,0);
 INSERT INTO `mob_groups` VALUES (61,4088,33,'Ulxzomit',330,0,3231,0,0,68,71,0); -- Follower Ul'Xzomit
-INSERT INTO `mob_groups` VALUES (61,8006,33,'Qnxzomit_jol',0,128,0,0,0,80,80,0);
+INSERT INTO `mob_groups` VALUES (62,8006,33,'Qnxzomit_jol',0,128,0,0,0,80,80,0);
 
 -- ------------------------------------------------------------
 -- Grand_Palace_of_HuXzoi (Zone 34)
