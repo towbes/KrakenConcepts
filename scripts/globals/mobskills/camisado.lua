ea755c2fb4f276efb1566907dc88ff1f44f9e14a
Umeboshi <terrandragon@gmail.com>
2023-05-01 17:26:05 -0700

    Diabolos Mobskill Audit

---
+++ scripts/globals/mobskills/camisado.lua
@@ -0,0 +1,31 @@
+-- -------------------------------------------------
+-- Camisado
+-- Deals damage to a single target. Additional effect: Knockback
+-- -------------------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+require("scripts/globals/mobskills")
+-- -------------------------------------------
+local mobskillObject = {}
+
+mobskillObject.onMobSkillCheck = function(target, mob, skill)
+    return 0
+end
+
+mobskillObject.onMobWeaponSkill = function(target, mob, skill)
+    local numhits = 1
+    local accmod = 1
+    local dmgmod = 1
+
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.magicalTpBonus.NO_EFFECT)
+
+    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.BLUNT, info.hitslanded)
+
+    if not skill:hasMissMsg() then
+        target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.BLUNT)
+    end
+
+    return dmg
+end
+
+return mobskillObject
