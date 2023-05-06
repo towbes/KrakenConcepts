5ca03d1f87820abb6b257a24787d8ced9ccec7e2
Umeboshi <terrandragon@gmail.com>
2023-05-05 18:45:31 -0700

    Quest Fixes

--- scripts/zones/Garlaige_Citadel/mobs/Guardian_Statue.lua
+++ scripts/zones/Garlaige_Citadel/mobs/Guardian_Statue.lua
@@ -1,38 +1,20 @@
 -----------------------------------
 -- Area: Garlaige Citadel
---   NM: Guardian Statue
--- Involved in Quest: Peace for the Spirit
+--  Mob: Funnel Bats
+--  Quest: Peace for the Spirit
 -----------------------------------
-require("scripts/globals/mobs")
-require("scripts/globals/quests")
-require("scripts/globals/status")
+require("scripts/globals/regimes")
 -----------------------------------
 local entity = {}
 
-entity.onMobInitialize = function(mob)
-    mob:setMobMod(xi.mobMod.IDLE_DESPAWN, 300)
-    mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
-end
-
-entity.onAdditionalEffect = function(mob, target, damage)
-    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.HP_DRAIN, {chance = 50, power = math.random(450, 550)})
-end
-
 entity.onMobSpawn = function(mob)
-    mob:setMod(xi.mod.DOUBLE_ATTACK, 10)
-    mob:setMod(xi.mod.REGAIN, 150)
-end 
+    mob:setMod(xi.mod.UDMGPHYS, -4000)
+    mob:setMod(xi.mod.UDMGMAGIC, -4000)
+end
 
-entity.onMobFight = function(mob, target)
-    if mob:getTP() >= 1000 then
-        mob:useMobAbility()
-    end
-
-    if mob:getHPP() <= 50 then
-        mob:setMod(xi.mod.REGAIN, 250)
-    elseif mob:getHPP() <= 25 then
-        mob:setMod(xi.mod.REGAIN, 400)
-    end
+entity.onMobFight = function(mob)
+    -- Regain gets stronger as fight goes on. ~75% = 87.5, ~50% = 175, ~25% = 262.5
+    mob:setMod(xi.mod.REGAIN, (100 - mob:getHPP()) * 3.5)
 end
 
 entity.onMobDeath = function(mob, player, optParams)
