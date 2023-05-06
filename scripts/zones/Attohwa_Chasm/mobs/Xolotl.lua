2d6932c7142919f4c19623560b06d811e69f3d6d
Umeboshi <terrandragon@gmail.com>
2023-05-06 02:42:36 -0700

    Xolotl Fixes

--- scripts/zones/Attohwa_Chasm/mobs/Xolotl.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Xolotl.lua
@@ -3,6 +3,8 @@
 --  Mob: Xolotl
 -----------------------------------
 require("scripts/globals/titles")
+require("scripts/globals/follow")
+-----------------------------------
 local ID = require("scripts/zones/Attohwa_Chasm/IDs")
 -----------------------------------
 local entity = {}
@@ -11,7 +13,7 @@ entity.onMobSpawn = function(mob)
     mob:setLocalVar("xolotlDead", 0)
 end
 
-entity.onMobFight = function(mob,target)
+entity.onMobFight = function(mob, target)
     local timeInterval = mob:getBattleTime() % 6
 
     -- Spawn skeleton pets
@@ -21,32 +23,30 @@ entity.onMobFight = function(mob,target)
             if target and child:getCurrentAction() == xi.act.ROAMING then -- doing nothing, make share enmity
                 child:updateEnmity(target)
             end
-        elseif mob:getCurrentAction() ~= 30 and mob:actionQueueEmpty() and timeInterval == (i-1)*3 then -- not spawned, not casting, not using an ability and should summon
+        elseif -- not spawned, not casting, not using an ability and should summon
+                mob:getCurrentAction() ~= xi.act.MAGIC_CASTING and
+                mob:actionQueueEmpty() and
+                timeInterval == (i - 1) * 3
+        then
             mob:setMagicCastingEnabled(false)
+            mob:setAutoAttackEnabled(false)
+            mob:setMobMod(xi.mobMod.NO_MOVE, 1)
             mob:entityAnimationPacket("casm")
             mob:timer(1500, function(xolotl)
                 xolotl:entityAnimationPacket("shsm")
                 xolotl:setMagicCastingEnabled(true)
+                mob:setAutoAttackEnabled(true)
+                mob:setMobMod(xi.mobMod.NO_MOVE, 0)
                 local pos = xolotl:getPos()
                 child:setSpawn(pos.x + i, pos.y - 0.5, pos.z - i, pos.rot)
                 child:spawn()
+                xi.follow.follow(child, mob)
             end)
         end
     end
 end
 
 entity.onMobRoam = function(mob)
-    local mobId = mob:getID()
-
-    for i = 1, 2 do
-        local child = GetMobByID(mobId + i)
-        if child:isSpawned() and child:getID() == mobId + 1 then
-            child:pathTo(mob:getXPos() + 1, mob:getYPos() + 3, mob:getZPos() + 0.15)
-        elseif child:isSpawned() and child:getID() == mobId + 2 then
-            child:pathTo(mob:getXPos() + 3, mob:getYPos() + 5, mob:getZPos() + 0.15)
-        end
-    end
-
     local hour = VanadielHour()
     if hour >= 4 and hour < 20 then -- Despawn Xolotl if its day
         DespawnMob(mob:getID())
@@ -62,8 +62,14 @@ end
 
 entity.onMagicHit = function(caster, target, spell)
     -- Sets max sleep resist if a light based sleep lands on Xolotl
-    if spell:tookEffect() and caster:isPC() and (spell:getID() == 376 or
-    spell:getID() == 463 or spell:getID() == 576 or spell:getID() == 584) then
+    if
+        spell:tookEffect() and
+        caster:isPC() and
+        (spell:getID() == 376 or
+        spell:getID() == 463 or
+        spell:getID() == 576 or
+        spell:getID() == 584)
+    then
         target:setMod(xi.mod.SLEEPRES, 100)
     end
 end
@@ -74,18 +80,13 @@ entity.onMobDeath = function(mob, player, optParams)
     mob:setLocalVar("xolotlDead", 1)
 end
 
-
 entity.onMobDespawn = function(mob)
-    -- Do not respawn Xolotl for 21-24 hours
-    mob:setRespawnTime(math.random(75600, 86400), true)
     -- Xolotl respawn timer only triggers on death, will respawn next game night if not defeated
     local xolotlDead = mob:getLocalVar("xolotlDead")
+    local cooldown = math.random(75600, 86400) -- 21h to 24h
 
     if xolotlDead == 1 then
-        UpdateNMSpawnPoint(mob:getID())
-        local respawn = math.random(75600, 86400) -- 21h to 24h
-        mob:setRespawnTime(respawn)
-        mob:setLocalVar("xolotlRespawn",(os.time() + respawn))
+        xi.mob.nmTODPersist(mob, cooldown)
         DisallowRespawn(ID.mob.XOLOTL, true)
     elseif xolotlDead == 0 then
         DisallowRespawn(ID.mob.XOLOTL, true)
