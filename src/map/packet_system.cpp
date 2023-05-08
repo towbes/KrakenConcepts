3a4c0f9686d899208cda02a5f127cfda69072060
Umeboshi <terrandragon@gmail.com>
2023-05-07 19:25:40 -0700

    Bazaar Message "Fix" https://github.com/AirSkyBoat/AirSkyBoat/pull/547/

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -729,6 +729,11 @@ void SmallPacket0x015(map_session_data_t* const PSession, CCharEntity* const PCh
             {
                 float distanceTravelled = distance(PChar->m_previousLocation.p, PChar->loc.p);
                 PChar->m_charHistory.distanceTravelled += static_cast<uint32>(distanceTravelled);
+
+                if (PChar->bazaar.timer <= time(NULL))
+                {
+                    PChar->pushPacket(new CBazaarMessagePacket(PChar));
+                }
             }
         }
 
@@ -6383,6 +6388,8 @@ void SmallPacket0x0DD(map_session_data_t* const PSession, CCharEntity* const PCh
                 {
                     PTarget->pushPacket(new CMessageStandardPacket(PChar, 0, 0, MsgStd::Examine));
                 }
+
+                PChar->bazaar.timer = time(NULL) + 30;
 
                 PChar->pushPacket(new CBazaarMessagePacket(PTarget));
                 PChar->pushPacket(new CCheckPacket(PChar, PTarget));
