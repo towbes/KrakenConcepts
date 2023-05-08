b1def5583359a6c568b998df532a49335732eb47
Umeboshi <terrandragon@gmail.com>
2023-05-08 06:41:46 -0700

    Mammet Battle Fix

--- scripts/globals/items/bottle_of_yellow_liquid.lua
+++ scripts/globals/items/bottle_of_yellow_liquid.lua
@@ -13,6 +13,8 @@ itemObject.onItemCheck = function(target)
     -- TODO: Can this ONLY be used on Mammet types?
     if target:hasStatusEffect(xi.effect.FOOD) then
         result = xi.msg.basic.IS_FULL
+    elseif target:getFamily() ~= 503 then
+        result = xi.msg.basic.ITEM_UNABLE_TO_USE
     end
 
     return result
