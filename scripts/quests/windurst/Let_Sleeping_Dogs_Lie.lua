8c6fbfcde477f567aea10ff13e6cd9e6acb59a5d
Umeboshi <terrandragon@gmail.com>
2023-05-11 03:12:45 -0700

    Quest Fix https://github.com/AirSkyBoat/AirSkyBoat/pull/1846/

--- scripts/quests/windurst/Let_Sleeping_Dogs_Lie.lua
+++ scripts/quests/windurst/Let_Sleeping_Dogs_Lie.lua
@@ -2,7 +2,11 @@
 -- Let Sleeping Dogs Die
 -----------------------------------
 -- !addquest 2 46
--- Mashuu-Ajuu 130 -5 167
+-- Paku-Nakku !gotoid 17752127
+-- Maabu-Sonbu !gotoid 17760275
+-- ??? QM 1 !gotoid 17224342
+-- Pechiru-Mashiru !gotoid 17752116
+
 -----------------------------------
 require('scripts/globals/interaction/quest')
 require('scripts/globals/npc_util')
@@ -17,6 +21,7 @@ quest.reward =
     item = xi.items.HYPNO_STAFF,
     fame = 75,
     fameArea = xi.quest.fame_area.WINDURST,
+    title = xi.title.SPOILSPORT,
 }
 
 quest.sections =
@@ -25,7 +30,7 @@ quest.sections =
         check = function(player, status, vars)
             return status == QUEST_AVAILABLE and
             player:getFameLevel(xi.quest.fame_area.WINDURST) >= 4 and
-            not player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.REAP_WHAT_YOU_SOW) == QUEST_ACCEPTED
+            player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.REAP_WHAT_YOU_SOW) ~= QUEST_ACCEPTED
         end,
 
         [xi.zone.WINDURST_WATERS] =
@@ -48,23 +53,28 @@ quest.sections =
 
         [xi.zone.WINDURST_WATERS] =
         {
-            ['Mashuu-Ajuu'] = quest:progressEvent(483),
-            ['Akkeke'] = quest:progressEvent(484),
-            ['Kirara'] = quest:progressEvent(485),
-            ['Rukuku'] = quest:progressEvent(487),
-            ['Koko Lihzeh'] = quest:progressEvent(488),
-            ['Chomoro-Kyotoro'] = quest:progressEvent(489),
+            ['Mashuu-Ajuu'] = quest:event(483),
+            ['Akkeke'] = quest:event(484),
+            ['Kirara'] = quest:event(485),
+            ['Rukuku'] = quest:event(487),
+            ['Koko Lihzeh'] = quest:event(488),
+            ['Chomoro-Kyotoro'] = quest:event(489),
             ['Paku-Nakku'] =
             {
-                onTrigger = function(player, npc)
-                    if quest:getVar(player, 'Prog') == 1 then
-                        return quest:progressEvent(494, 653393087, 1, 0, 1200, 8388608, 3749, 4095, 2) -- 0, 0, 0, 0)
-                    elseif quest:getVar(player, 'Prog') == 3 then
-                        return quest:progressEvent(499)
-                    else
-                        return quest:progressEvent(482)
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, xi.items.BUNCH_OF_BLAZING_PEPPERS) and quest:getVar(player, 'Prog') == 1 then
+                        return quest:progressEvent(494)
                     end
                 end,
+
+                onTrigger = function(player, npc)
+                    if quest:getVar(player, 'Prog') == 3 then
+                        return quest:progressEvent(499)
+                    else
+                        return quest:event(482)
+                    end
+                end,
+
             },
             ['Pechiru-Mashiru'] =
             {
@@ -72,7 +82,7 @@ quest.sections =
                     if quest:getVar(player, 'Prog') == 2 then
                         return quest:progressEvent(495, 4155, 1102, 1, 50617851, 2729760, 0)
                     elseif quest:getVar(player, 'Prog') == 3 then
-                        return quest:progressEvent(496)
+                        return quest:event(496)
                     elseif quest:getVar(player, 'Prog') == 4 then
                         return quest:progressEvent(497)
                     end
@@ -88,7 +98,7 @@ quest.sections =
                     quest:setVar(player, 'Prog', 3)
                 end,
                 [497] = function(player, csid, option, npc)
-                    quest:completeQuest(player)
+                    quest:complete(player)
                 end,
                 [499] = function(player, csid, option, npc)
                     if option == 2 then
@@ -106,9 +116,9 @@ quest.sections =
                     if quest:getVar(player, 'Prog') == 0 then
                         return quest:progressEvent(319, 653392653, 4155, 1102, 1020) -- 0, 0, 0, 0)
                     elseif quest:getVar(player, 'Prog') == 2 then
-                        return quest:progressEvent(321, 653393447, 1102, 0, 0, 67108863, 41513848, 4095, 4)
+                        return quest:event(321, 653393447, 1102, 0, 0, 67108863, 41513848, 4095, 4)
                     else
-                        return quest:progressEvent(320, 653392653, 4155, 1102) -- 1020, 0, 0, 0, 0)
+                        return quest:event(320, 653392653, 4155, 1102) -- 1020, 0, 0, 0, 0)
                     end
                 end,
             },
@@ -117,6 +127,33 @@ quest.sections =
             {
                 [319] = function(player, csid, option, npc)
                     quest:setVar(player, 'Prog', 1)
+                end,
+            },
+        },
+
+        [xi.zone.PASHHOW_MARSHLANDS] =
+        {
+            ['qm1'] =
+            {
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, xi.items.SICKLE) then
+                        local chance = math.random(1, 5)
+                        if chance <= 2 then
+                            npcUtil.giveItem(player, xi.items.BUNCH_OF_BLAZING_PEPPERS)
+                            player:tradeComplete(false)
+                            return player:startEvent(12, 1102, 0, 0, xi.items.BUNCH_OF_BLAZING_PEPPERS)-- successful no break
+                        elseif chance == 3 then
+                            npcUtil.giveItem(player, xi.items.BUNCH_OF_BLAZING_PEPPERS)
+                            player:confirmTrade()
+                            return player:startEvent(12, 1102, 1, 0, xi.items.BUNCH_OF_BLAZING_PEPPERS) -- successful but breaks
+                        elseif chance == 4 then
+                            player:confirmTrade()
+                            return player:startEvent(12, 0, 1, 0) -- unsuccessful and breaks
+                        elseif chance == 5 then
+                            player:tradeComplete(false)
+                            return player:startEvent(12, 0, 0, 0) -- unsuccessful and no break
+                        end
+                    end
                 end,
             },
         },
@@ -133,7 +170,7 @@ quest.sections =
             {
                 onTrigger = function(player, npc)
                     if player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.HAT_IN_HAND) ~= QUEST_ACCEPTED then
-                        quest:progressEvent(498)
+                        quest:event(498)
                     end
                 end,
             },
