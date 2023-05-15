85812f72911623f67d236582b6541250d8d8748d
Umeboshi <terrandragon@gmail.com>
2023-05-15 13:45:03 -0700

    DRG AF2 Quest Fix https://github.com/AirSkyBoat/AirSkyBoat/pull/2785/

--- scripts/zones/Port_San_dOria/npcs/Ceraulian.lua
+++ scripts/zones/Port_San_dOria/npcs/Ceraulian.lua
@@ -42,9 +42,9 @@ entity.onTrigger = function(player, npc)
     elseif
         quotasStatus == QUEST_AVAILABLE and
         player:getMainJob() == xi.job.DRG and
-        player:getMainLvl() >= xi.settings.main.AF1_QUEST_LEVEL or
+        player:getMainLvl() >= xi.settings.main.AF2_QUEST_LEVEL or
         player:getSubJob() == xi.job.DRG and
-        player:getSubLvl() >= xi.settings.main.AF1_QUEST_LEVEL and
+        player:getSubLvl() >= xi.settings.main.AF2_QUEST_LEVEL and
         quotasNo == 0
     then
         player:startEvent(18) -- Long version of quest start
