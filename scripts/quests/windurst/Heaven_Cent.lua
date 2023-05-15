d31f5f1f06445cf9bcfce0adea2b184984c3e31a
Umeboshi <terrandragon@gmail.com>
2023-05-15 07:21:59 -0700

    Quest Fixes https://github.com/AirSkyBoat/AirSkyBoat/pull/2702/

--- scripts/quests/windurst/Heaven_Cent.lua
+++ scripts/quests/windurst/Heaven_Cent.lua
@@ -9,7 +9,7 @@ require('scripts/globals/npc_util')
 require('scripts/globals/quests')
 require('scripts/globals/zone')
 -----------------------------------
-ID = require('scripts/zones/Maze_of_Shakhrami/IDs')
+local ID = require('scripts/zones/Maze_of_Shakhrami/IDs')
 -----------------------------------
 
 local quest = Quest:new(xi.quest.log_id.WINDURST, xi.quest.id.windurst.HEAVEN_CENT)
@@ -58,45 +58,19 @@ quest.sections =
             },
         },
     },
-
     {
         check = function(player, status, vars)
-            return status == QUEST_ACCEPTED
+            return status == QUEST_ACCEPTED and
+            vars.Prog == 0
         end,
 
         [xi.zone.WINDURST_WATERS] =
         {
             ['Ropunono'] =
             {
-                onTrigger = function(player, npc)
-                    if quest:getVar(player, 'Prog') == 1 then
-                        return quest:progressEvent(289, 0, 557, xi.items.SHELLING_PIECE, 0, 0, 4095, 4)
-                    else
-                        return quest:progressEvent(296)
-                    end
-                end,
-
                 onTrade = function(player, npc, trade)
-                    if trade:hasItemQty(557, 1) and trade:getItemCount() == 1 and quest:getVar(player, 'Prog') == 0 then
-                        return quest:progressEvent(288, 0, 557, xi.items.SHELLING_PIECE, 0, 0, 0, 0, 4)
-
-                    -- Fake Shelling Piece
-                    elseif
-                        trade:hasItemQty(xi.items.SHELLING_PIECE, 1) and
-                        trade:getItemCount() == 1 and
-                        quest:getVar(player, 'Prog') == 1 and
-                        quest:getVar(player, 'Coin') == 0
-                    then
-                        return quest:progressEvent(296, 0, 0, xi.items.SHELLING_PIECE, 0, 0, 0, 4095, 4)
-
-                    -- Real Shelling Piece
-                    elseif
-                        trade:hasItemQty(xi.items.SHELLING_PIECE, 1) and
-                        trade:getItemCount() == 1 and
-                        quest:getVar(player, 'Prog') == 1 and
-                        quest:getVar(player, 'Coin') == 1
-                    then
-                        return quest:progressEvent(292, 0, 0, xi.items.SHELLING_PIECE, 0, 0, 0, 4095, 4)
+                    if npcUtil.tradeHasExactly(trade, xi.items.AHRIMAN_LENS) then
+                        return quest:progressEvent(288, 0, xi.items.AHRIMAN_LENS, xi.items.SHELLING_PIECE)
                     end
                 end,
             },
@@ -104,32 +78,76 @@ quest.sections =
             onEventFinish =
             {
                 [288] = function(player, csid, option, npc)
+                    player:confirmTrade()
                     quest:setVar(player, 'Prog', 1)
                 end,
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_ACCEPTED and
+            vars.Prog == 1
+        end,
+
+        [xi.zone.WINDURST_WATERS] =
+        {
+            ['Ropunono'] =
+            {
+                onTrigger = function(player, npc)
+                    return quest:event(289, 0, xi.items.AHRIMAN_LENS, xi.items.SHELLING_PIECE)
+                end,
+
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, xi.items.SHELLING_PIECE) then
+                        if quest:getVar(player, 'Coin') == 1 then
+                            -- Real Shelling Piece
+                            return quest:progressEvent(292, 0, 0, xi.items.SHELLING_PIECE)
+                        else
+                            -- Fake Shelling Piece
+                            return quest:progressEvent(296, 0, 0, xi.items.SHELLING_PIECE)
+                        end
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
                 [292] = function(player, csid, option, npc)
-                    player:tradeComplete()
+                    player:confirmTrade()
                     quest:complete(player)
                 end,
+
                 [296] = function(player, csid, option, npc)
                     -- Reset vars to allow players to search for another coin
                     quest:setVar(player, 'firstCoin', 0)
                     quest:setVar(player, 'secondCoin', 0)
                     quest:setVar(player, 'thirdCoin', 0)
-                    player:tradeComplete()
+                    player:confirmTrade()
                 end,
             },
         },
 
         [xi.zone.MAZE_OF_SHAKHRAMI] =
         {
+            -- Iron Door
             ['_5i0'] =
             {
                 onTrade = function(player, npc, trade)
-                    if trade:hasItemQty(543, 1) and trade:getItemCount() == 1 and quest:getVar(player, 'Prog') == 1 then
+                    if npcUtil.tradeHasExactly(trade, xi.items.RUSTY_KEY) then
                         return quest:progressEvent(41)
                     end
                 end,
+
+                onTrigger = function(player, npc)
+                    if player:getPos().x >= 248 then
+                        return quest:progressEvent(42)
+                    end
+                end,
             },
+
+            -- Chests
             ['_5i4'] =
             {
                 onTrigger = function(player, npc)
@@ -138,21 +156,21 @@ quest.sections =
                     -- Only allow if player doesn't have coin
                     if not player:hasItem(xi.items.SHELLING_PIECE) then
                         if rand > 0.2 then
-                            player:messageSpecial(incorrect[math.random(1,4)])
+                            player:messageSpecial(incorrect[math.random(1, 4)])
                             return quest:progressEvent(47)
 
                         -- Do not reveal correct coin if player has seen correct coin from this chest.
-                        elseif quest:getVar(player, 'firstCoin') == 0
-                        then
-                            player:messageSpecial(correct[math.random(1,4)])
+                        elseif quest:getVar(player, 'firstCoin') == 0 then
+                            player:messageSpecial(correct[math.random(1, 4)])
                             quest:setVar(player, 'firstCoin', 1)
                             return quest:progressEvent(46)
                         end
                     else
-                        player:messageSpecial(ID.text.CHEST_EMPTY)
+                        return quest:messageSpecial(ID.text.CHEST_EMPTY)
                     end
                 end,
             },
+
             ['_5i5'] =
             {
                 onTrigger = function(player, npc)
@@ -161,20 +179,21 @@ quest.sections =
                     -- Only allow if player doesn't have coin
                     if not player:hasItem(xi.items.SHELLING_PIECE) then
                         if rand > 0.2 then
-                            player:messageSpecial(incorrect[math.random(1,4)])
+                            player:messageSpecial(incorrect[math.random(1, 4)])
                             return quest:progressEvent(49)
 
                         -- Do not reveal correct coin if player has seen correct coin from this chest.
                         elseif quest:getVar(player, 'secondCoin') == 0 then
-                            player:messageSpecial(correct[math.random(1,4)])
+                            player:messageSpecial(correct[math.random(1, 4)])
                             quest:setVar(player, 'secondCoin', 1)
                             return quest:progressEvent(48)
                         end
                     else
-                        player:messageSpecial(ID.text.CHEST_EMPTY)
+                        return quest:messageSpecial(ID.text.CHEST_EMPTY)
                     end
                 end,
             },
+
             ['_5i6'] =
             {
                 onTrigger = function(player, npc)
@@ -183,87 +202,63 @@ quest.sections =
                     -- Only allow if player doesn't have coin
                     if not player:hasItem(xi.items.SHELLING_PIECE) then
                         if rand > 0.2 then
-                            player:messageSpecial(incorrect[math.random(1,4)])
+                            player:messageSpecial(incorrect[math.random(1, 4)])
                             return quest:progressEvent(51)
 
                         -- Do not reveal correct coin if player has seen correct coin from this chest.
                         elseif quest:getVar(player, 'thirdCoin') == 0 then
-                            player:messageSpecial(correct[math.random(1,4)])
+                            player:messageSpecial(correct[math.random(1, 4)])
                             quest:setVar(player, 'thirdCoin', 1)
                             return quest:progressEvent(50)
                         end
                     else
-                        player:messageSpecial(ID.text.CHEST_EMPTY)
+                        return quest:messageSpecial(ID.text.CHEST_EMPTY)
                     end
                 end,
             },
 
             onEventFinish =
             {
-                -- Correct coin
+                -- Opening iron door
+                [41] = function(player, csid, option, npc)
+                    player:confirmTrade()
+                end,
+
+                -- Correct coin (Flag correct coin)
                 [46] = function(player, csid, option, npc)
-                    if option == 2 then
-                        if player:getFreeSlotsCount() == 0 then
-                            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, xi.items.SHELLING_PIECE)
-                        else
-                            quest:setVar(player, 'Coin', 1)
-                            player:addItem(xi.items.SHELLING_PIECE)
-                        end
+                    if option == 2 and npcUtil.giveItem(player, xi.items.SHELLING_PIECE) then
+                        quest:setVar(player, 'Coin', 1)
                     end
                 end,
-                -- Incorrect coin
-                [47] = function(player, csid, option, npc)
-                    if option == 2 then
-                        if player:getFreeSlotsCount() == 0 then
-                            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, xi.items.SHELLING_PIECE)
-                        else
-                            quest:setVar(player, 'Coin', 0)
-                            player:addItem(xi.items.SHELLING_PIECE)
-                        end
-                    end
-                end,
-                -- Correct coin
+
                 [48] = function(player, csid, option, npc)
-                    if option == 2 then
-                        if player:getFreeSlotsCount() == 0 then
-                            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, xi.items.SHELLING_PIECE)
-                        else
-                            quest:setVar(player, 'Coin', 1)
-                            player:addItem(xi.items.SHELLING_PIECE)
-                        end
+                    if option == 2 and npcUtil.giveItem(player, xi.items.SHELLING_PIECE) then
+                        quest:setVar(player, 'Coin', 1)
                     end
                 end,
+
+                [50] = function(player, csid, option, npc)
+                    if option == 2 and npcUtil.giveItem(player, xi.items.SHELLING_PIECE) then
+                        quest:setVar(player, 'Coin', 1)
+                    end
+                end,
+
                 -- Inorrect coin
                 [49] = function(player, csid, option, npc)
                     if option == 2 then
-                        if player:getFreeSlotsCount() == 0 then
-                            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, xi.items.SHELLING_PIECE)
-                        else
-                            quest:setVar(player, 'Coin', 0)
-                            player:addItem(xi.items.SHELLING_PIECE)
-                        end
+                        npcUtil.giveItem(player, xi.items.SHELLING_PIECE)
                     end
                 end,
-                -- Correct coin
-                [50] = function(player, csid, option, npc)
+
+                [47] = function(player, csid, option, npc)
                     if option == 2 then
-                        if player:getFreeSlotsCount() == 0 then
-                            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, xi.items.SHELLING_PIECE)
-                        else
-                            quest:setVar(player, 'Coin', 1)
-                            player:addItem(xi.items.SHELLING_PIECE)
-                        end
+                        npcUtil.giveItem(player, xi.items.SHELLING_PIECE)
                     end
                 end,
-                -- Incorrect coin
+
                 [51] = function(player, csid, option, npc)
                     if option == 2 then
-                        if player:getFreeSlotsCount() == 0 then
-                            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, xi.items.SHELLING_PIECE)
-                        else
-                            quest:setVar(player, 'Coin', 0)
-                            player:addItem(xi.items.SHELLING_PIECE)
-                        end
+                        npcUtil.giveItem(player, xi.items.SHELLING_PIECE)
                     end
                 end,
             },
