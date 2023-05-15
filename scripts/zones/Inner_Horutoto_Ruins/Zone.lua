3a9786ec5dd02dcd0c802cab21b002d21af0ac43
Umeboshi <terrandragon@gmail.com>
2023-05-15 03:08:04 -0700

    NM TOD Persist: Sarutabaruta

--- scripts/zones/Inner_Horutoto_Ruins/Zone.lua
+++ scripts/zones/Inner_Horutoto_Ruins/Zone.lua
@@ -14,6 +14,9 @@ zoneObject.onInitialize = function(zone)
     zone:registerTriggerArea(3, -257.8, 0, -24.9, -256.1, 1, -23.5) -- Black
     zone:registerTriggerArea(4, -261, -3, 182, -257, -1, 186) -- Teleport at H-6
 
+    -- NM Persistence
+    xi.mob.nmTODPersistCache(zone, ID.mob.MALTHA)
+
     xi.treasure.initZone(zone)
 end
 
