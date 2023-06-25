4a9204a6a32dc8289dc29bb99e915e754ea29769
Cynica <CynciaGaming@gmail.com>
2023-06-24 21:19:52 -0400

    Updated Login Campaign to better match prior changes and updated prize list to include mounts.

--- scripts/globals/events/login_campaign_data.lua
+++ scripts/globals/events/login_campaign_data.lua
@@ -15,169 +15,174 @@ local prizes =
         {
             1126, -- Beastmen's Seal
             1127, -- Kindred's Seal
-            2955, -- Kindred's Crest
-            2956, -- High Kindred's Crest
-            2957, -- Sacred Kindred's Crest
-            -- 1857, -- Cordial Invite
-            -- 2306, -- Martial Ball Invite
-            -- 5364, -- Training Grounds Key
-            -- 2487, -- Mercenary Camp Entry Slip
-            -- 5741, -- Flask Of Pest Repellent
-            -- 3557, -- Athena Orb
-            -- 5113, -- Cracked Nut
-            -- 3541, -- Seasoning Stone
-            -- 3543, -- Fossilized Fang
-            -- 3542, -- Fossilized Bone
-            -- 5724, -- Pungent Powder
-            -- 6535, -- Pungent Powder II
-            -- 9890, -- Tarazacum Orb
+            4107, -- Earth Cluster
+            4109, -- Water Cluster
+            4106, -- Wind Cluster
+            4104, -- Fire Cluster
+            4105, -- Ice Cluster
+            4108, -- Lightning Cluster
+            4110, -- Light Cluster
+            4111, -- Dark Cluster
+            -- 2955, -- Kindred's Crest
+            -- 2956, -- High Kindred's Crest
+            -- 2957, -- Sacred Kindred's Crest
         },
     },
 
-    [5] =
+    [5] = 
     {
         ["price"] = 100,
         ["items"] =
         {
-            8734,  -- Mog Kupon I-S1
-            8966,  -- Eudaemon Blade
-            8967,  -- Eudaemon Cape
-            8968,  -- Eudaemon Ring
-            8969,  -- Eudaemon Sash
-            8970,  -- Eudaemon Shield
-            17006, -- Drill Calamary
-            17007, -- Dwarf Pugil
-            6413,  -- Astral Cube
-            -- 9891,  -- Zinnia Orb
-            -- 10112, -- Cipher: Zeid
-            -- 10113, -- Cipher: Lion
-            10118, -- Cipher: Naja
-            10120, -- Cipher: Lehko
-            -- 10124, -- Cipher: Luzaf
-            -- 10125, -- Cipher: Najelith
-            -- 10129, -- Cipher: Domina
-            -- 10134, -- Cipher: S. Sibyl
-            -- 10142, -- Cipher: Karaha
-            -- 10149, -- Cipher: Areuhat
+            4176, -- Mysterious Gift
+            1456, -- 100 Byne Bill
+            1453, -- M. Silverpiece
+            1450, -- L. Jadeshell
+            2488, -- Alexandrite
+            
         },
     },
 
     [9] =
     {
-        ["price"] = 100,
+        ["price"] = 250,
         ["items"] =
-        {
-            -- 10136, -- Cipher: Uka
-            -- 10141, -- Cipher: Kuyin
-            -- 10144, -- Cipher: Abenzio
-            -- 10145, -- Cipher: Rughadjeen
-            -- 10150, -- Cipher: Lhe
-            -- 10151, -- Cipher: Mayakov
-            -- 10155, -- Cipher: Brygid
-            -- 10156, -- Cipher: Mildaurion
-            -- 10161, -- Cipher: Rongelouts
-            -- 10166, -- Cipher: Robel-Akbel
-            -- 10178, -- Cipher: Ullegore
-            -- 10179, -- Cipher: Teodor
-            -- 10183, -- Cipher: Darrcuiln
-            20713, -- Excalipoor
-            6008,  -- Copse Candy
+        {   
+            558, -- Tarut:The Fool
+            559, -- Tarut:The Death
+            561, -- Tarut:The King
+            562, -- Tarut:The Hermit
+
         },
     },
 
     [13] =
     {
-        ["price"] = 300,
-        ["items"] =
-        {
-            -- 10187, -- Cipher: Shantotto II
-            10069, -- ♪Goobbue
-            10051, -- ♪Crab
-            10058, -- ♪Beetle
-            -- TODO: The other mounts
-            10384, -- Cumulus Masque
-            20666, -- Blizzard Brand
-            -- 25658, -- Wyrm. Masque +1
-            -- 25757, -- Wyrmking Suit +1
-            -- 5854,  -- Frayed Pouch (B)
-            -- 5855,  -- Frayed Pouch (A)
-            -- 5856,  -- Frayed Pouch (G)
-            -- 5857,  -- Frayed Pouch (D)
-            -- 5858,  -- Frayed Pouch (R)
-            -- 5946,  -- Frayed Sack (D)
-            -- 5947,  -- Frayed Sack (L)
-            -- 4064,  -- Rem's Tale Ch.1
-            -- 4065,  -- Rem's Tale Ch.2
-            -- 4066,  -- Rem's Tale Ch.3
-            -- 4067,  -- Rem's Tale Ch.4
-            -- 4068,  -- Rem's Tale Ch.5
-        },
-    },
-
-    [17] =
-    {
         ["price"] = 500,
         ["items"] =
         {
-            -- TODO: Special month-to-month things
-            10073, -- ♪Dhalmel
+            1418, -- Gem of the East
+            1420, -- Gem of the South
+            1422, -- Gem of the West
+            1424, -- Gem of the North
+            1850, -- First Virtue
+            1853, -- Second Virtue
+            1856, -- Third Virtue
+
+    },
+},
+
+    [17] =
+    {
+        ["price"] = 750,
+        ["items"] =
+        {
+            1419, -- Springstone
+            1421, -- Summerstone
+            1423, -- Autumnstone
+            1425, -- Winterstone
+            3339, -- Honey Wine
+            3341, -- Beastly Shank
+            3343, -- Blue Pondweed
+            1873, -- Brigand's Chart
+            1874, -- Pirate's Chart
         },
 
     },
 
     [21] =
     {
-        ["price"] = 750,
+        ["price"] = 1500,
         ["items"] =
         {
-            3885, -- Melodious Plans
-            3886, -- Timbre Case Kit
-            3887, -- Musichinery Kit
-            -- 9079, -- Kitchen Brick
-            -- 9080, -- Kitchen Stove
-            -- 9081, -- Kitchen Plate
-            3339, -- Honey Wine
-            3341, -- Beastly Shank
-            3343, -- Blue Pondweed
-            -- 1873, -- Brigand's Chart
-            -- 1874, -- Pirate's Chart
-            -- 6381, -- Fisherman's Feast
-            -- 4069, -- Copy Of Rem's Tale, Chapter 6
-            -- 4070, -- Copy Of Rem's Tale, Chapter 7
-            -- 4071, -- Copy Of Rem's Tale, Chapter 8
-            -- 4072, -- Copy Of Rem's Tale, Chapter 9
-            -- 4073, -- Copy Of Rem's Tale, Chapter 10
+
+            1907, -- Silver Chip
+            1908, -- Cerulean Chip
+            1909, -- Smalt Chip
+            1910, -- Smoky Chip
+            1986, -- Orchid Chip
+            1987, -- Charcoal Chip
+            1988, -- Magenta Chip
+            1851, -- Dead of Placidity
+            1854, -- Dead of Moderation
+            1870, -- Dead of Sensibility
+            3340, -- Sweet Tea
+            3342, -- Savory Shank
+            3344, -- Red Pondweed
+            
+
         },
     },
 
     [25] =
     {
-        ["price"] = 1000,
+        ["price"] = 2500,
         ["items"] =
         {
-            -- 6499,  -- Patio Design Plans
-            26165, -- Facility Ring
-            26164, -- Caliber Ring
-            -- 6486,  -- Frayed Sack (Pel)
-            -- 6487,  -- Frayed Sack (Fer)
-            -- 6488,  -- Frayed Sack (Tau)
+            10430, -- Decennial Crown 
+            10431, -- Decennial Tiara
+            10429, -- Moogle Masque
+            10250, -- Moogle Suit
+            25657, -- Wyrmking Masque
+            25756, -- Wyrmking Suit
+            27765, -- Chocobo Masque
+            27911, -- Chocobo Suit
+            18464, -- Ark Tachi
+            18545, -- Ark Tabar
+            18563, -- Ark Scythe
+            18912, -- Ark Saber 
+            18913, -- Ark Sword
+            20573, -- Aern Dagger
+            20674, -- Aern Sword
+            21742, -- Aern Axe
+            21860, -- Aern Spear
+            22065, -- Aern Staff
         },
     },
 
     [29] =
     {
-        ["price"] = 1500,
+        ["price"] = 4000,
         ["items"] =
         {
-            3340, -- Sweet Tea
-            3342, -- Savory Shank
-            3344, -- Red Pondweed
-            -- 8720, -- Maliya. Coral Orb
-            -- 8722, -- Hepatizon Ingot
-            -- 8724, -- Beryllium Ingot
-            -- 8726, -- Exalted Lumber
-            -- 8728, -- Sif's Macrame
+        -- 11538, -- Nexus Cape
+        -- 15554, -- Pelican Ring
+        -- 26166, -- Invisible Ring
+        -- 26167, -- Sneak Ring
+        -- 26169, -- Reraise Ring
+        -- 10924, -- Chocobo Torque
+        -- 10925, -- Fisher's Torque
+        -- 10926, -- Field Torque 
+        -- 10948, -- Carver's Torque
+        -- 10949, -- Smith's Torque
+        -- 10950, -- Goldsm. Torque
+        -- 10951, -- Weaver's Torque
+        -- 10952, -- Taner's Torque 
+        -- 10953, -- Bone Torque 
+        -- 10954, -- Alchemist Torque 
+        -- 10955, -- Culin. Torque 
+			10050, -- ♪Tiger
+			10051, -- ♪Crab
+			10052, -- ♪Red Crab
+			10053, -- ♪Bomb
+			10054, -- ♪Sheep
+			10055, -- ♪Morbol
+			10056, -- ♪Crawler
+			10058, -- ♪Beetle
+			10059, -- ♪Moogle
+			10061, -- ♪Tulfaire
+			10062, -- ♪Warmachine
+			10063, -- ♪Xzmoit
+			10064, -- ♪Hippogryph
+			10065, -- ♪Spectral Chair
+			10066, -- ♪Spheroid
+			10068, -- ♪Coeurl
+			10071, -- ♪Levitus
+			10069, -- ♪Goobbue
+			10070, -- ♪Raaz
         },
     },
+
 }
+
 return prizes
