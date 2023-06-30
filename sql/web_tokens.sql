34f82244498a74e8cf720e42997ac763f6d54da8
Cynica <CynicaGaming@gmail.com>
2023-06-30 12:23:50 -0400

    Emptied table web_tokens.sql

--- sql/web_tokens.sql
+++ sql/web_tokens.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.6.8-MariaDB - mariadb.org binary distribution
+-- Host:                         play.cactuarxi.com
+-- Server version:               10.6.14-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL Version:             11.3.0.6295
 -- --------------------------------------------------------
@@ -18,12 +18,10 @@ CREATE TABLE IF NOT EXISTS `web_tokens` (
   `token` varchar(32) NOT NULL,
   `expiration` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Dumping data for table xidb.web_tokens: ~0 rows (approximately)
 /*!40000 ALTER TABLE `web_tokens` DISABLE KEYS */;
-INSERT INTO `web_tokens` (`id`, `token`, `expiration`) VALUES
-	(1000, '5807c512f51f9b7d491a8d53174f6903', '2023-06-30 12:07:34');
 /*!40000 ALTER TABLE `web_tokens` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
