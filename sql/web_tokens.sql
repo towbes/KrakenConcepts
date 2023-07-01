99c019ccc58b352455ae0ea7c6b0edf69d2db8ad
Cynica <CynciaGaming@gmail.com>
2023-06-30 20:59:41 -0400

    Modified web_tokens.sql with additional 'type' column.

--- sql/web_tokens.sql
+++ sql/web_tokens.sql
@@ -1,29 +1,22 @@
 -- --------------------------------------------------------
--- Host:                         play.cactuarxi.com
+-- Host:                         127.0.0.1
 -- Server version:               10.6.14-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.3.0.6295
+-- HeidiSQL Version:             12.3.0.6589
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping structure for table xidb.web_tokens
-CREATE TABLE IF NOT EXISTS `web_tokens` (
-  `id` int(11) NOT NULL,
-  `token` varchar(32) NOT NULL,
-  `expiration` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
 -- Dumping data for table xidb.web_tokens: ~0 rows (approximately)
-/*!40000 ALTER TABLE `web_tokens` DISABLE KEYS */;
-/*!40000 ALTER TABLE `web_tokens` ENABLE KEYS */;
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
