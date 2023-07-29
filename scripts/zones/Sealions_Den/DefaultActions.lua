4650fd7e50cb8a04ef22c4a48bcb8ff71fc0ce61
Umeboshi <terrandragon@gmail.com>
2023-07-28 18:07:12 -0700

    The Warriors Path Bug Test

--- scripts/zones/Sealions_Den/DefaultActions.lua
+++ scripts/zones/Sealions_Den/DefaultActions.lua
@@ -1,6 +1,6 @@
 local ID = require("scripts/zones/Sealions_Den/IDs")
 
 return {
-    ['_0w0']    = { messageSpecial = ID.text.IRON_GATE_LOCKED },
+    -- ['_0w0']    = { messageSpecial = ID.text.IRON_GATE_LOCKED },
     ['Sueleen'] = { event = 20 },
 }
