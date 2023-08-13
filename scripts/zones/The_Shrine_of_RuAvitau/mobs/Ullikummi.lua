5f9d62857e26bdec3e7173c6128a7053d0b0dac7
Umeboshi <terrandragon@gmail.com>
2023-08-13 11:35:35 -0700

    Ulli Hotfix

--- scripts/zones/The_Shrine_of_RuAvitau/mobs/Ullikummi.lua
+++ scripts/zones/The_Shrine_of_RuAvitau/mobs/Ullikummi.lua
@@ -3,26 +3,24 @@
 --  Mob: Ullikummi
 -----------------------------------
 local ID = require("scripts/zones/The_Shrine_of_RuAvitau/IDs")
-require("scripts/globals/status")
 require("scripts/globals/mobs")
 -----------------------------------
 local entity = {}
 
 -- TODO: Heavy Strike should ALWAYS knockback its target regardless of if it hits or does damage.
 
-entity.onMobInitialize = function(mob)
-    mob:addMod(xi.mod.REGAIN, 1000)
-
+--[[entity.onMobInitialize = function(mob)
     mob:addListener("WEAPONSKILL_USE", "ULLI_WEAPONSKILL_USE", function(mobArg, target, wsid, tp, action)
-        if action:param(target:getID()) > 1 then
+        if action:getParam(target:getID()) > 1 then
             mobArg:resetEnmity(target)
         end
     end)
-end
+end]]
 
 entity.onMobSpawn = function(mob)
     mob:setMobMod(xi.mobMod.SKILL_LIST, 135)
     mob:setMobMod(xi.mobMod.SPELL_LIST, 0)
+    mob:setMod(xi.mod.REGAIN, 1000)
 end
 
 entity.onMobDespawn = function(mob)
