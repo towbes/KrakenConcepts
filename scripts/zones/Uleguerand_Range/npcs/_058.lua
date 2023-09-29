f10683b6837b904fd9b51a8dbbb5409114746b02
Umeboshi <terrandragon@gmail.com>
2023-09-29 04:56:51 -0700

    Uleguerand Range Frozen Waterfall Bypass Implements a system much like the barriers in Ifrits Cauldron. Players can trade 6 Fire Clusters to lower the ice wall.

---
+++ scripts/zones/Uleguerand_Range/npcs/_058.lua
@@ -0,0 +1,18 @@
+-----------------------------------
+-- Area: Uleguerand Range
+--  NPC: Frozen Waterfall
+--  Receive Chamnaet Ice upon trading Cotton Pouch
+-----------------------------------
+local ID = require("scripts/zones/Uleguerand_Range/IDs")
+require("scripts/globals/keyitems")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+
+end
+
+return entity
