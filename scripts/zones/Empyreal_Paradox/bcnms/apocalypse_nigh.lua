fbbb5c462c2cf58a03397ae70c0381839efa1a13
Umeboshi <terrandragon@gmail.com>
2023-09-29 18:38:51 -0700

    Apoc Nigh Bug Fix

--- scripts/zones/Empyreal_Paradox/bcnms/apocalypse_nigh.lua
+++ scripts/zones/Empyreal_Paradox/bcnms/apocalypse_nigh.lua
@@ -6,30 +6,27 @@ require("scripts/globals/battlefield")
 require("scripts/globals/quests")
 -----------------------------------
 local battlefieldObject = {}
-
 battlefieldObject.onBattlefieldTick = function(battlefield, tick)
     xi.battlefield.onBattlefieldTick(battlefield, tick)
 end
-
 battlefieldObject.onBattlefieldRegister = function(player, battlefield)
 end
-
 battlefieldObject.onBattlefieldEnter = function(player, battlefield)
 end
-
 battlefieldObject.onBattlefieldLeave = function(player, battlefield, leavecode)
     if leavecode == xi.battlefield.leaveCode.WON then
         local _, clearTime, partySize = battlefield:getRecord()
-        player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), 0)
+        local arg8 = (player:getQuestStatus(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.APOCALYPSE_NIGH) == QUEST_COMPLETED) and 1 or 0
+        player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), arg8)
     elseif leavecode == xi.battlefield.leaveCode.LOST then
         player:startEvent(32002)
     end
 end
-
-battlefieldObject.onEventUpdate = function(player, csid, option, npc)
+battlefieldObject.onEventUpdate = function(player, csid, option)
 end
-
-battlefieldObject.onEventFinish = function(player, csid, option, npc)
+battlefieldObject.onEventFinish = function(player, csid, option)
+    if csid == 7 then
+        player:setPos(-0.004, -10, -465.053, 64, 33)
+    end
 end
-
 return battlefieldObject
