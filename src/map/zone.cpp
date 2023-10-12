e6a8461d75c1d34dc04ce63f8c6e901a74036117
Umeboshi <terrandragon@gmail.com>
2023-10-12 01:02:31 -0700

    Zone timeout 5s to 300s. Allows mobs time to path home from zoneline.

--- src/map/zone.cpp
+++ src/map/zone.cpp
@@ -925,7 +925,7 @@ void CZone::ZoneServer(time_point tick)
         m_BattlefieldHandler->HandleBattlefields(tick);
     }
 
-    if (ZoneTimer && m_zoneEntities->CharListEmpty() && m_timeZoneEmpty + 5s < server_clock::now())
+    if (ZoneTimer && m_zoneEntities->CharListEmpty() && m_timeZoneEmpty + 300s < server_clock::now())
     {
         ZoneTimer->m_type = CTaskMgr::TASK_REMOVE;
         ZoneTimer         = nullptr;
