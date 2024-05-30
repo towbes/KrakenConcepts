0b35be1d448801cbe03e90468d5875e1903827a7
Umeboshi <terrandragon@gmail.com>
2024-05-30 05:07:28 -0700

    [core] Add Skillchain Binding for Mobskills. https://github.com/LandSandBoat/server/pull/5292

--- src/map/lua/luautils.cpp
+++ src/map/lua/luautils.cpp
@@ -3856,7 +3856,7 @@ namespace luautils
         return 0;
     }
 
-    int32 OnMobWeaponSkill(CBaseEntity* PTarget, CBaseEntity* PMob, CMobSkill* PMobSkill, action_t* action)
+    int32 OnMobWeaponSkill(CBaseEntity* PTarget, CBaseEntity* PMob, CMobSkill** PMobSkill, action_t* action)
     {
         TracyZoneScoped;
 
@@ -3868,7 +3868,7 @@ namespace luautils
             auto onMobWeaponSkill = lua["xi"]["zones"][zone]["mobs"][name]["onMobWeaponSkill"];
             if (onMobWeaponSkill.valid())
             {
-                auto result = onMobWeaponSkill(CLuaBaseEntity(PTarget), CLuaBaseEntity(PMob), CLuaMobSkill(PMobSkill), CLuaAction(action));
+                auto result = onMobWeaponSkill(CLuaBaseEntity(PTarget), CLuaBaseEntity(PMob), CLuaMobSkill(*PMobSkill), CLuaAction(action));
                 if (!result.valid())
                 {
                     sol::error err = result;
@@ -3879,7 +3879,7 @@ namespace luautils
         }
 
         // Mob Skill Script
-        auto mobskill_name = PMobSkill->getName();
+        auto mobskill_name = (*PMobSkill)->getName();
 
         auto onMobWeaponSkill = lua["xi"]["actions"]["mobskills"][mobskill_name]["onMobWeaponSkill"];
         if (!onMobWeaponSkill.valid())
@@ -3887,7 +3887,7 @@ namespace luautils
             return 0;
         }
 
-        auto result = onMobWeaponSkill(CLuaBaseEntity(PTarget), CLuaBaseEntity(PMob), CLuaMobSkill(PMobSkill));
+        auto result = onMobWeaponSkill(CLuaBaseEntity(PTarget), CLuaBaseEntity(PMob), CLuaMobSkill(*PMobSkill));
         if (!result.valid())
         {
             sol::error err = result;
